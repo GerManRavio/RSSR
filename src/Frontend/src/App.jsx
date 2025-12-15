ec139d1ffdc083100dc7da66c4dd405788be0137
GerManRavio <elias.trzoska@icloud.com>
2025-12-15 07:23:07 +0100

    tailwind stuff

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -8,8 +8,24 @@ export default function App() {
     }
     return (
         <>
-            <h1>Home</h1>
-            <button>Login</button>
+            <header>
+                <nav>
+                    <menu className="flex gap-4 p-3 border rounded-sm">
+                        <li className="hover:underline">
+                            <a href="/">Home</a>
+                        </li>
+                        <li className="hover:underline">
+                            <a href="/User">User</a>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
+            <main>
+                <section className="p-3">
+                    <h1 className="text-2xl font-semibold">Home</h1>
+                    <button className="hover:bg-sky-700 bg-sky-800 rounded-sm p-1">Login</button>
+                </section>
+            </main>
         </>
     )
 }
